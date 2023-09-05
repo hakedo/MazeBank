2f73362b0ebdf9e9db3145cf6181cce7d8894fab
Edgar Hakobyan <hak3do@gmail.com>
2023-09-04 20:19:32 -0700

    Update App.java

--- src/main/java/com/example/mazebank/App.java
+++ src/main/java/com/example/mazebank/App.java
@@ -1,12 +1,17 @@
 package com.example.mazebank;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
